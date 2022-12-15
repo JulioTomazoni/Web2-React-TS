2e98a274435dce91f67942560b4ba7246a8c9564
JulioTomazoni <juliotomazoni18@gmail.com>
2022-12-14 22:44:23 -0300

    Added styles in tables

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,44 @@ import './App.css'
 import { Components } from './components';
 
 function App() {
-
+  const [data, getData] = useState([]);
+  const URL = "https://jsonplaceholder.typicode.com/posts";
+ 
+  useEffect(() => {
+    fetchData();
+  }, []);
+ 
+  const fetchData = () => {
+    fetch(URL)
+      .then((res) => res.json())
+ 
+      .then((response) => {
+        console.log(response);
+        getData(response);
+      });
+  };
+  
   return (
-    <div className="App">
-      <Components/>
+    <div>
+      <h1>How Display API data in Table in React JS</h1>
+      <table>
+        <tr>
+          <th>User ID</th>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Descripation</th>
+        </tr>
+        {data.map((item, i) => (
+          <tr key={i}>
+            <td>{item.userId}</td>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.body}</td>
+          </tr>
+        ))}
+      </table>
     </div>
-  )
+  );
 }
 
 export default App
