ac532c218b82c069b1dd21cddefc8046ca222265
JulioTomazoni <juliotomazoni18@gmail.com>
2022-12-14 21:47:23 -0300

    Add Json-Server

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,11 @@ import './App.css'
 
 import { Components } from './components';
 
-interface Repository {
-  full_name: string,
-  description: string,
-}
-
 function App() {
-  const [repositories, setRepositories] = useState<Repository[]>([]);
-
-  useEffect(() =>
-    {
-      fetch('http://localhost:3000')
-      .then(res => res.json())
-      .then(data => {
-        setRepositories(data);
-      })
-    },
-    []
-  )
 
   return (
     <div className="App">
-      <ul>
-        {repositories.map(repo => {
-          return (
-              <li key={repo.full_name}>
-                <strong>{repo.full_name}</strong>
-                <p>{repo.description}</p>
-              </li>
-            )
-        })}
-      </ul>
+      <Components/>
     </div>
   )
 }
